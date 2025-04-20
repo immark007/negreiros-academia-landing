e624663b6569027936a42aa25ace047af4fba1a7
immark007 <immark5507@gmail.com>
2025-04-20 11:49:33 -0300

    feat: adicionando component de pricing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
 import { About } from "./components/About"
 import { Equipment } from "./components/Equipment"
+import { Pricing } from "./components/Pricing"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Hero />
     <About />
     <Equipment />
+    <Pricing />
     </>
   )
 }
